bcc616c1b2de643f46a57dc419a72b6f8c6f5c8f
fahimad24 <mdtaraquerahmanfahim@gmail.com>
2026-04-08 01:42:30 +0600

    react project with vite, tailwind, react router etc

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl text-pink-700 text-center" s>
+          Welcome to Book Vibe
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
